98a95763efbd87570338b65ddee4fc27c174688d
ReenuR <82420557+ReenuR@users.noreply.github.com>
2025-04-17 10:53:26 -0700

    Time: 1 ms (99.58%) | Memory: 41.4 MB (96.63%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,19 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        char[] charArray = s.toCharArray();
-        for(int i = 0; i<charArray.length; i++){
-            if(stack.isEmpty()){
-                stack.push(charArray[i]);    
-            }else{
-                if(stack.peek()=='{' && charArray[i] == '}' ||
-                stack.peek()=='[' && charArray[i] == ']'||
-                stack.peek()=='(' && charArray[i] == ')')
-                stack.pop();
-                else
-                    stack.push(charArray[i]);
-            }
+        int n = s.length();
+    // Quick reject: odd length can't be fully paired
+    if ((n & 1) == 1) return false;
+
+    char[] stack = new char[n];
+    int top = 0;
+
+    for (int i = 0; i < n; i++) {
+        char c = s.charAt(i);
+        if (top > 0 && isMatchingPair(stack[top - 1], c)) {
+            // pop matching open bracket
+            top--;
+        } else {
+            // push new bracket (open or unmatched close)
+            stack[top++] = c;
         }
-        return stack.size() == 0; 
     }
+    return top == 0;
+}
+
+private boolean isMatchingPair(char open, char close) {
+    // single comparison of three possibilities
+    return (open == '(' && close == ')')
+        || (open == '[' && close == ']')
+        || (open == '{' && close == '}');
+}
 }
