bc0098a2dfea26c52eac52d97067941ac0921a12
ReenuR <82420557+ReenuR@users.noreply.github.com>
2025-05-12 13:45:37 -0700

    Time: 2 ms (98.81%) | Memory: 45 MB (46.31%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // we can store element and its index in hashmap and we'll loop thru array and check if complement is present in the array or not  if yes just return the current index and map's value.
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i =0; i<nums.length; i++){
             int complement = target - nums[i];
             if(map.containsKey(complement))
-                return new int[]{map.get(complement), i};
+                return new int[]{i, map.get(complement)};
             map.put(nums[i], i);
         }
         return new int[] {};
-    }
+      }
 }
