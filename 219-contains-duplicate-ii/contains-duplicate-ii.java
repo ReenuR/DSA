d931423c0599832a2ca686eead666f08b0992343
ReenuR <82420557+ReenuR@users.noreply.github.com>
2025-06-06 11:12:04 -0700

    Time: 28 ms (15.84%) | Memory: 58.1 MB (93.54%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-         // need to scan an array till I find a duplicate. and then  I 've to check whether abs(i-j) <=k
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0, j =0; i<nums.length; i++){
-            // #expand the window
-            int num = nums[i];
-            map.put(num, map.getOrDefault(num, 0) + 1);
-
-            while(map.get(num) > 1){
-                if(nums[j] == num && Math.abs(i-j) <= k)
-                    return true;
-                map.put(nums[j], map.get(nums[j])- 1);
-                j++;
+       Map<Integer, Integer> freq = new HashMap<>();
+       for(int i=0; i<nums.length; i++){
+            if(freq.containsKey(nums[i]))
+                return true;
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0)+1);
+            if(freq.size()>k){
+                freq.remove(nums[i-k]);
             }
-            
-        }
-        return false;
+       }
+       return false;
     }
 }
