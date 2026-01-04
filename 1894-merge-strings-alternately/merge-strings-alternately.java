3f27ddf7538123b8eb45634c22db8e5ab2ad1fbe
ReenuR <82420557+ReenuR@users.noreply.github.com>
2026-01-04 11:29:29 -0800

    Time: 1 ms (95.35%) | Memory: 43 MB (76.57%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,45 +1,18 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int length = word1.length() + word2.length();
-        char[] mergedArr = new char[length];
-        char[] str1 = word1.toCharArray();
-        char[] str2 = word2.toCharArray();
-        int i=0, j=0, k=0;
-        while( i<word1.length() && j< word2.length())  {
-            mergedArr[k] = str1[i++];
-            mergedArr[++k] = str2[j++];
-            k++;
-        } 
-        
-        while(i<word1.length()){
-            mergedArr[k++] = str1[i++];
-        }
-        while(j< word2.length()){
-            mergedArr[k++] = str2[j++];
-        }
 
-        return new String(mergedArr);
+        StringBuffer sb = new StringBuffer();
+        int i=0, j=0;
+        for(; i<word1.length() && j< word2.length(); i++, j++){
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(j));
+        }
+        if(i< word1.length()){
+            sb.append(word1.substring(i));
+        }
+        if(j < word2.length()){
+            sb.append(word2.substring(j));
+        }
+        return sb.toString();
     }
 }
-
-
-/* Aug 16th 2023
-int n = word1.length() + word2.length();
-        char str3 [] = new char[n];
-        char str1[] = word1.toCharArray();
-        char str2[] = word2.toCharArray();
-        for (int i=0, j=0; j<n; i++,j++ ){
-                if(i< word1.length() && i < word2.length()){
-                    str3[j] = str1[i];
-                    str3[++j] = str2[i];
-                }
-                else{
-                    if(i < word1.length())
-                    str3[j] = str1[i];
-                    else if(i < word2.length())
-                    str3[j] = str2[i];
-        
-                }
-        }
-        return new String (str3);
-*/
