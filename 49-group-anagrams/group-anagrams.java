86ba435d99e5df03a6ef435a5b63203626bacff2
ReenuR <82420557+ReenuR@users.noreply.github.com>
2026-02-01 10:00:36 -0800

    Time: 7 ms (76.26%) | Memory: 49.5 MB (82.50%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(int i = 0; i<strs.length; i++){
+            char[] charArray = strs[i].toCharArray();
+            Arrays.sort(charArray);
+            String sorted = new String(charArray);
+            if(map.containsKey(sorted)){
+                map.get(sorted).add(strs[i]);
+            }
+            else{
+                map.put(sorted, new ArrayList<>());
+                map.get(sorted).add(strs[i]);
+            }
+        }
+
+    return new ArrayList<>(map.values());
+    }
+}
