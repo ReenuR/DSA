345477659a19bf3350814fbee7e927cdc8acafd4
ReenuR <82420557+ReenuR@users.noreply.github.com>
2026-02-01 10:02:06 -0800

    Time: 6 ms (99.25%) | Memory: 49.7 MB (71.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -5,14 +5,14 @@ class Solution {
         for(int i = 0; i<strs.length; i++){
             char[] charArray = strs[i].toCharArray();
             Arrays.sort(charArray);
+
             String sorted = new String(charArray);
-            if(map.containsKey(sorted)){
-                map.get(sorted).add(strs[i]);
-            }
-            else{
-                map.put(sorted, new ArrayList<>());
-                map.get(sorted).add(strs[i]);
-            }
+
+            if(!map.containsKey(sorted)){
+                 map.put(sorted, new ArrayList<>());
+            }    
+            map.get(sorted).add(strs[i]);
+            
         }
 
     return new ArrayList<>(map.values());
