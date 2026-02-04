6a4c962efc164e3c04fc7a30cfd5893aa5185a90
ReenuR <82420557+ReenuR@users.noreply.github.com>
2026-02-04 15:20:39 -0800

    Time: 53 ms (77.17%) | Memory: 70.4 MB (63.50%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,26 +1,29 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        // return sum of all the values present in map
+          int maxLen = -1;
 
-        Map<Integer, Integer> basket = new HashMap<>();
-        int left = 0, right =0;
-        int maxFruits = 0;
-        
-        while(right < fruits.length){
-            int fruit = fruits[right];
-            basket.put(fruit, basket.getOrDefault(fruit, 0)+1);
+        HashMap<Integer, Integer> basket = new HashMap<>();
+        int start = 0, end =0;
+        for( int l = 0, r = 0;r<fruits.length; r++){
+            int fruit = fruits[r];
 
-            while(basket.size() > 2){
-                basket.put(fruits[left],basket.get(fruits[left])-1);
-                if(basket.get(fruits[left]) == 0){
-                    basket.remove(fruits[left]);
-                }
-                left++;
+            basket.put(fruit, basket.getOrDefault(fruit,0)+1);
+
+            while(basket.size()>2){
+                int fruitLeft =fruits[l]; 
+                basket.put(fruitLeft, basket.get(fruitLeft)-1);
+                if(basket.get(fruitLeft) == 0)
+                    basket.remove(fruitLeft);
+                l++;
             }
-            maxFruits = Math.max(maxFruits, right - left + 1);;
-            right++;
-            
+            maxLen = Math.max(maxLen, r-l+1);
+
         }
-        return maxFruits;
+        return maxLen;
     }
 }
+/**
+
+OP--> max no.of trees i visit(fruits[i])
+
+ */
