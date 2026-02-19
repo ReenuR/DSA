9d0f750b709702c615d01d6985fb6ff9b691c669
ReenuR <82420557+ReenuR@users.noreply.github.com>
2026-02-19 09:22:34 -0800

    Time: 1 ms (99.70%) | Memory: 94.7 MB (13.40%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,33 +1,21 @@
 class Solution {
+  
     public int maxProfit(int[] prices) {
+        int minVal = Integer.MAX_VALUE;
         int maxProfit = 0;
-        int minSoFar = prices[0];
-        // I need to track minimum value till today and see profit from it and store it. 
+        for(int i = 0; i<prices.length; i++){
+                if(prices[i]<minVal)
+                    minVal=prices[i];
 
-        for(int i=1; i<prices.length; i++){
-            
-            minSoFar = Math.min(minSoFar, prices[i]);
-
-            maxProfit = Math.max(maxProfit, prices[i]-minSoFar);
-
+                maxProfit =  Math.max(maxProfit, prices[i] - minVal);
         }
         return maxProfit;
-
     }
 }
 
-/** int maxProfit = -1;
-        int profit = 0;
-        int currentProfit = 0;
 
-        for(int i=0; i< prices.length; i++){
-            for (int j = i+1; j<prices.length; j++){
 
-                if(prices[i] < prices[j]){
-                    currentProfit = prices[j] - prices[i];
-                }
-                profit = Math.max(profit, currentProfit);
-            }
-            maxProfit = Math.max(maxProfit, profit);
-        }
-        return maxProfit; */
+
+//If you plot out the values of the array into a line chart, you'll see that you're basically just looking for a start point that is the lowest point on the graph and an end point which is the highest. So as you're iterating through the array, there's only two possible cases
+
+//
