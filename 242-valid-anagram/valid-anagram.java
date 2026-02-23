0616e61872a495901319900b63f7976b79203110
ReenuR <82420557+ReenuR@users.noreply.github.com>
2026-02-23 10:17:40 -0800

    Time: 6 ms (42.53%) | Memory: 44.3 MB (83.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length())
-        return false;
+            return false;
 
-        char ch;
-        int[] freqArray = new int[26];
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-        for(int i=0; i<sArray.length; i++){
-            ch = sArray[i];
-            freqArray[ch-'a']++;
+        int [] arr = new int[26];
+        for(int i=0; i<s.length(); i++){
+            char charAtS = s.charAt(i);
+            char charAtT = t.charAt(i);
+            arr[charAtS-'a']++;
+            arr[charAtT-'a']--;
         }
-        for(int i = 0; i<tArray.length; i++){
-            ch = tArray[i];
-            if(--freqArray[ch-'a'] < 0)
+
+        for(int i =0; i<arr.length; i++){
+            if(arr[i] !=0)
                 return false;
         }
-        
+
         return true;
+
     }
 }
