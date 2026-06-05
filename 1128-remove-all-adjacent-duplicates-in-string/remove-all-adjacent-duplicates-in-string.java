3ff61bb22443bbca2bfcfca3629a5730bf04af43
ReenuR <82420557+ReenuR@users.noreply.github.com>
2026-06-05 11:00:07 -0700

    Time: 21 ms (69.55%) | Memory: 47.2 MB (39.07%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(int i = 0; i< s.length(); i++){
+            char curr = s.charAt(i);
+            if(stack.isEmpty()){
+                stack.push(curr);
+            }
+            else {
+                if(curr == stack.peek())
+                {
+                    stack.pop();
+                }else{
+                    stack.push(curr);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
